879ce53843f22fcd146be4c664c132a44ea7ee32
Quan Hua <quanhua92@gmail.com>
2017-03-11 22:37:41 +0700

    + Update

---
+++ src/common.py
@@ -0,0 +1,5 @@
+import string
+
+DIGITS = string.digits
+LETTERS = string.ascii_uppercase
+CHARS = LETTERS + DIGITS
--- src/generate.py
+++ src/generate.py
@@ -1,12 +1,41 @@
-import string
 import random
+import os
+import numpy
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import common
+
+FONT_DIR = "./fonts"
+FONT_HEIGHT = 32  # Pixel size to which the chars are resized
+
+OUTPUT_SHAPE = (64, 128)
+
+CHARS = common.CHARS + " "
+
+
+def make_char_ims(font_path, output_height):
+    font_size = output_height * 4
+
+    font = ImageFont.truetype(font_path, font_size)
+
+    height = max(font.getsize(c)[1] for c in CHARS)
+
+    for c in CHARS:
+        width = font.getsize(c)[0]
+        im = Image.new("RGBA", (width, height), (0, 0, 0))
+
+        draw = ImageDraw.Draw(im)
+        draw.text((0, 0), c, (255, 255, 255), font=font)
+        scale = float(output_height) / height
+        im = im.resize((int(width * scale), output_height), Image.ANTIALIAS)
+        yield c, numpy.array(im)[:, :, 0].astype(numpy.float32) / 255.
 
 
 def generate_code():
     length = random.choice([8, 9, 10])
     has_dot = random.random() > 0.5
-    letters = string.ascii_uppercase
-    digits = string.digits
     if has_dot is True:
         is_dash = random.random() > 0.5
         if is_dash:
@@ -14,35 +43,45 @@ def generate_code():
         else:
             format_string = "{}{}.{}{}.{}{}.{}{}"
         code = format_string.format(
-            random.choice(letters),
-            random.choice(letters),
-            random.choice(digits),
-            random.choice(digits),
-            random.choice(letters),
-            random.choice(letters+digits),
-            random.choice(digits),
-            random.choice(digits),
+            random.choice(common.LETTERS),
+            random.choice(common.LETTERS),
+            random.choice(common.DIGITS),
+            random.choice(common.DIGITS),
+            random.choice(common.LETTERS),
+            random.choice(common.CHARS),
+            random.choice(common.DIGITS),
+            random.choice(common.DIGITS),
         )
     else:
         code = "{}{} {}{} {}{} {}{}".format(
-            random.choice(letters),
-            random.choice(letters),
-            random.choice(digits),
-            random.choice(digits),
-            random.choice(letters),
-            random.choice(letters+digits),
-            random.choice(digits),
-            random.choice(digits),
+            random.choice(common.LETTERS),
+            random.choice(common.LETTERS),
+            random.choice(common.DIGITS),
+            random.choice(common.DIGITS),
+            random.choice(common.LETTERS),
+            random.choice(common.CHARS),
+            random.choice(common.DIGITS),
+            random.choice(common.DIGITS),
         )
     if length == 8:
         return code
     elif length == 9:
         code += "{}".format(
-            random.choice(digits),
+            random.choice(common.DIGITS),
         )
         return code
     else:
-        code += "{}{}".format(random.choice(digits), random.choice(digits))
+        code += "{}{}".format(random.choice(common.DIGITS), random.choice(common.DIGITS))
         return code
 
+
+def load_fonts(folder_path):
+    font_char_ims = {}
+    fonts = [f for f in os.listdir(folder_path) if f.endswith('.ttf')]
+    for font in fonts:
+        font_char_ims[font] = dict(make_char_ims(os.path.join(folder_path, font), FONT_HEIGHT))
+    return fonts, font_char_ims
+
 print(generate_code())
+ims = make_char_ims("./fonts/UKNumberPlate.ttf", FONT_HEIGHT)
+print(ims)
